e0cc4bf3854896f0abb7ad0d149cb3b5413d2579
Shubham Patrick <ahthneeuhl@gmail.com>
2023-11-07 11:13:22 +0530

    setting up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,12 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
+
 const Issues: React.FC = () => {
-  return <div>Issues</div>;
+  return (
+    <Flex direction="row" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>New Issue</Button>
+    </Flex>
+  );
 };
 
 export default Issues;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { Theme } from "@radix-ui/themes";
 import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
