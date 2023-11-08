87804053c70a9532432c05d373ce6f5c22b348ab
Shubham Patrick <ahthneeuhl@gmail.com>
2023-11-08 15:58:25 +0530

    add remaining loading skeleton

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
 import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Loading: React.FC = () => {
-  return <Box>Loading...</Box>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+    </Box>
+  );
 };
 
 export default Loading;
