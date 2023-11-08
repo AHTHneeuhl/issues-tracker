3d100005de22a3414b01ecb902da6edb9ec0aa65
Shubham Patrick <ahthneeuhl@gmail.com>
2023-11-08 16:10:11 +0530

    create Skeleton components

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,3 +2,4 @@ export { default as ErrorMessage } from "./ErrorMessage";
 export { default as Spinner } from "./Spinner";
 export { default as IssueStatus } from "./IssueStatus";
 export { default as Link } from "./Link";
+export { default as Skeleton } from "./Skeleton";
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,7 +1,5 @@
-import { Box, Table } from "@radix-ui/themes";
-import { IssueStatus } from "../components";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Table } from "@radix-ui/themes";
 
 const Loading: React.FC = () => {
   const issues = [1, 2, 3, 4, 5];
